be1c957945cb3596ec0eedde747a5635b45ffd42
yangchen55 <tseyang986@gmail.com>
2023-01-23 11:25:48 +1100

    profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,23 +1,25 @@
-import express from "express"
-import { ERROR, SUCCESS } from "../constant.js"
-import { comparePassword, hashPassword } from "../helpers/BrcyptHelper.js"
-const router = express.Router()
+import express from "express";
+import { ERROR, SUCCESS } from "../constant.js";
+import { comparePassword, hashPassword } from "../helpers/BrcyptHelper.js";
+const router = express.Router();
 import {
   createUser,
   getAnyUser,
   getUserByEmail,
-} from "../models/userModel/UserModel.js"
+  getUserById,
+  updateUserById,
+} from "../models/userModel/UserModel.js";
 
 router.get("/", (req, res, next) => {
   try {
     res.json({
       status: SUCCESS,
       message: "todo get user",
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 // create new user
 router.post("/", async (req, res, next) => {
@@ -43,38 +45,38 @@ router.post("/", async (req, res, next) => {
   // }
 
   try {
-    const { email } = req.body
+    const { email } = req.body;
 
-    const userExists = await getUserByEmail(email)
+    const userExists = await getUserByEmail(email);
     if (userExists) {
       return res.json({
         status: "error",
         message: "User already exists. Please log in!",
-      })
+      });
     }
 
     // hash password
-    const hashPass = hashPassword(req.body.password)
+    const hashPass = hashPassword(req.body.password);
 
     if (hashPass) {
-      req.body.password = hashPass
-      const result = await createUser(req.body)
+      req.body.password = hashPass;
+      const result = await createUser(req.body);
 
       if (result?._id) {
         return res.json({
           status: SUCCESS,
           message: "User has been created successfully. You may now log in!",
-        })
+        });
       }
       return res.json({
         status: ERROR,
         message: "User not created. Please try again later!",
-      })
+      });
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 // login user
 router.post("/login", async (req, res, next) => {
@@ -99,33 +101,66 @@ router.post("/login", async (req, res, next) => {
   // }
 
   try {
-    const { email } = req.body
-    const user = await getUserByEmail(email)
+    const { email } = req.body;
+    const user = await getUserByEmail(email);
 
     if (user?._id) {
       // Check if password is valid
-      const isPassMatch = comparePassword(req.body.password, user.password)
+      const isPassMatch = comparePassword(req.body.password, user.password);
       if (isPassMatch) {
-        user.password = undefined
+        user.password = undefined;
         return res.json({
           status: SUCCESS,
           message: "Login successful",
           user,
-        })
+        });
       }
       res.json({
         status: ERROR,
         message: "Invalid password",
-      })
+      });
     } else {
       res.json({
         status: ERROR,
         message: "User not found!",
-      })
+      });
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default router
+router.patch("/password-update", async (req, res, next) => {
+  try {
+    const user = await getUserById(req.headers.authorization);
+    const { currentPassword } = req.body;
+    const passMatched = comparePassword(currentPassword, user?.password);
+    console.log(passMatched);
+    if (passMatched) {
+      const hashedPass = hashPassword(req.body.password);
+      if (hashedPass) {
+        const updatedPassword = await updateUserById(user._id, {
+          password: hashedPass,
+        });
+        if (updatedPassword?._id) {
+          return res.json({
+            status: "success",
+            message: "password updated successfuly",
+          });
+        }
+        return res.json({
+          status: "error",
+          message: "unable to update password",
+        });
+      }
+    }
+    return res.json({
+      status: "error",
+      message: "please enter correct password",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
